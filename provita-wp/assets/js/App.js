53a725740a3673aeb2bc956d68bdffa9be763f92
Leonardo Villegas <villegasleo18@gmail.com>
2023-11-27 10:06:58 -0600

    avance
    +Se realizaron los cambios de la lista de Fabi
    -El mapa no tiene clusters a este punto

--- provita-wp/assets/js/App.js
+++ provita-wp/assets/js/App.js
@@ -1,6 +1,42 @@
 (function($) {
     $(document).ready(function() {
-		console.log("ready");
+      // Cuando se hace clic en un botón con la clase "mi-boton"
+	$(".menu-btn").click(function () {
+		// Agrega la clase "transform" al div con el ID "mi-div"
+		$("#navigation").addClass("openPopUp");
+	});
+
+	$(".close-popup").click(function () {
+		// Agrega la clase "transform" al div con el ID "mi-div"
+		$("#navigation").removeClass("openPopUp");
+	});
+
+
+	initializeCarrouseles();
     });
 
 })(jQuery);
+
+
+function initializeCarrouseles(){
+	$('.owl-carousel').owlCarousel({
+		loop: true,
+		nav: false,
+		margin:10,
+		responsive: {
+			0: {
+				items: 2,
+
+			},
+			600: {
+				items: 3
+			},
+			820: {
+				items: 1
+			},
+			1000: {
+				items: 1
+			}
+		}
+	})
+}
